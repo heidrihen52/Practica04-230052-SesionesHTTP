cafae5870112766ecb3e81fce24a8c5b93ce0d97
Adrian Perez Jimenez <230052@utxicotepec.edu.mx>
2025-01-21 08:19:35 -0600

    Practica 04
    Creacion de los endpoints para realizar y revisar las sesiones, mostrando su informacion, y datos del cliente

--- app.js
+++ app.js
@@ -6,8 +6,112 @@ import moment from 'moment-timezone';
 import os from 'os';
 
 const app = express();
+//middleware para manejar datos codificados  en url
+app.use(express.urlencoded({extended:true}))
+//midleware para manejar datos json
+app.use(express.json())
+
+app.use(
+    session({
+        secret:'p4-APJ#pixelg7hadry-SesionesHTTP',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 5 * 60 * 1000} 
+    })
+)
+
+//sesiones almacenadas en memoria RAM
+const sessions={}
+//Funcion que nos permite acceder a la informacion de la interfaz de red en este caso LAN
+const getclienteIp = (req) => {
+    
+    return(
+        req.headers["x-forwarded-for"] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket?.remoteAddress
+    )
+};
 
 
+//Login endpoint
+app.post("/login", (req,res)=>{ 
+    console.log(req);
+    const {email,nickname,macAddress}=req.body;
+    if(!email || !nickname || !macAddress){
+        return res.status(400).json({message:"Se esperan campos requeridos"})
+    }
+    const sessionID =uuidv4();
+    const now = new Date();
+
+    session[sessionID]={
+        sessionID,
+        email,
+        nickname,
+        macAddress,
+        ip:getclienteIp(req),
+        createdAt:now,
+        lastAccessed:now
+
+    }
+
+    res.status(200).json({
+        message:"Se ha logueado de manera exitosa",
+        sessionID,
+    });
+});
+
+//logout enpoint
+app.post('/logout',(req,res)=> {
+    const {sessionID}=req.body;
+    if(!sessionID || !session[sessionID]){
+        return res.status(404).json({message:"No se ha encontrado sesion acticva"})
+    }
+    delete session[sessionID];
+    req.session.destroy((err)=>{
+        if(err){
+            return res.status(500).send('Error al cerrar la sesión');
+        }
+        })
+    res.status(200).json({message:"Sesion cerrada exitosamente"}); 
+})
+
+//actualizar la sesion
+app.put("/update",(req,res)=>{
+    const{sessionID,email,nickname}=req.body;
+    if(!sessionID || !session[sessionID]){
+        return res.status(404).json({message:"no existe una sesión activa"})
+    }
+
+    if(email) sessions[sessionID].email=email;
+    if(nickname) session[sessionID].nickname=nickname;
+    session[sessionID].lastAccessed=new Date();
+})
+
+//Estatus
+ app.get("/status", (req,res)=>{
+    const sessionID = req.query.sessionID;
+    
+    if(!sessionID || !session[sessionID]){
+        return res.status(404).json({message:"No hay sesion activa"})
+    }
+    res.status(200).json({
+        message:"sesion activa",
+        session:session[sessionID]
+    })
+ });
+
+ //Endpoint para listar las sesiones activas
+ 
+
+ ///configurar cors
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
+})
+
+/*
 // Configuración del middleware de sesiones
 
 app.use(
@@ -107,9 +211,4 @@ app.get('/cerrar-sesion', (req,res)=>{
         res.send('No hay una sesión activa para cerrar.')
     }
 })
-
-
-const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
-})
+*/
